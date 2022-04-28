1cea7b933b59dff7c2121e258339ec393641f448
Noel Carrasco <noel.carrasco@e-campus.uab.cat>
2022-04-28 19:06:40 +0200

    Exec Python Script PoC
    Ejecuta el script de python desde una función del backend de nodejs

--- Backend/app.js
+++ Backend/app.js
@@ -1,10 +1,35 @@
 
+const path = require('path');
 const express = require('express')
+const {spawn} = require('child_process');
+
 const app = express()
 const port = 3000
+const robot_script = path.join(__dirname, '/main.py');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//TODO: Comentar las funciones con el convenio JSDoc
+// Url: https://www.jetbrains.com/help/rider/Creating_JSDoc_Comments.html
+
+app.post('/texto', (req, res) => {
+  const FelipeTexto = req.body.texto
+})
+
+
+app.get('/camera', (req, res) => {
+  const python = spawn('python', [robot_script]);
+  var dataToSend;
+  
+  python.stdout.on('data', function (data) {
+   dataToSend = data.toString();
+  });
+
+  python.on('close', (code) => {
+    console.log(`child process close all stdio with code ${code}`);
+    res.send(dataToSend)
+  });
 })
 
 app.listen(port, () => {
--- Backend/main.py
+++ Backend/main.py
@@ -1,9 +1,17 @@
 from time import sleep
-from picamera import PiCamera
 
-camera = PiCamera()
-camera.resolution = (1024, 768)
-camera.start_preview()
-# Camera warm-up time
-sleep(2)
-camera.capture('foo.jpg')
+def foto():
+    from picamera import PiCamera
+    camera = PiCamera()
+    camera.resolution = (1024, 768)
+    camera.start_preview()
+    # Camera warm-up time
+    sleep(2)
+    camera.capture('foo.jpg')
+
+def test():
+    test = "var test"
+    print("print test")
+    return test
+    
+test()
