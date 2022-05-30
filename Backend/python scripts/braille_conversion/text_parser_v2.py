ee30a62595533d10a686202cb4805fcb3fbafaea
Andrés Bitto <anbitto@gmail.com>
2022-05-30 09:56:43 +0200

    codigo de impresión updated

--- Backend/python scripts/braille_conversion/text_parser_v2.py
+++ Backend/python scripts/braille_conversion/text_parser_v2.py
@@ -33,31 +33,31 @@ def count_mayus(palabra):
     return sum(1 for c in palabra if c.isupper())
 
 
-# Número de carácteres que caben en una linea
-LINE_CHAR_LIMIT = 12
-# Número de carácteres que caben en una linea
-paper_line_limit = 13
-# Número de carácteres que se llevan escritos
-page_buffer = 0
-# Número de carácteres que se llevan escritos
-line_buffer = 0
+def braille_formatting(text):
+    # Número de carácteres que caben en una linea
+    LINE_CHAR_LIMIT = 12
+    # Número de carácteres que caben en una linea
+    paper_line_limit = 13
+    # Número de carácteres que se llevan escritos
+    page_buffer = 0
+    # Número de carácteres que se llevan escritos
+    line_buffer = 0
 
-texto_de_ejemplo = "Reina en mi espíritu una alegría admirable, muy parecida a las dulces alboradas de la primavera, de que gozo "
+    # texto_de_ejemplo = "Reina en mi espíritu una alegría admirable, muy parecida a las dulces alboradas de la primavera, de que gozo "
 
-texto_de_ejemplo = "En un lugar de la Mancha, de cuyo nombre no quiero acordarme, no ha mucho tiempo que vivia un hidalgo de los de lanza en astillero, adarga antigua, rocin flaco y galgo corredor. Una olla de algo mas vaca que carnero, salpicon las mas noches, duelos y quebrantos los sabados, lentejas los viernes, algun palomino de anadidura los domingos, consumian las tres partes de su hacienda. El resto della concluian sayo de velarte, calzas de velludo para las fiestas con sus pantuflos de lo mismo, los dias de entre semana se honraba con su vellori de lo mas fino. Tenia en su casa una ama que pasaba de los cuarenta, y una sobrina que no llegaba a los veinte, y un mozo de campo y plaza, que asi ensillaba el rocin como tomaba la podadera. Frisaba la edad de nuestro hidalgo con los cincuenta anos, era de complexion recia, seco de carnes, enjuto de rostro; gran madrugador y amigo de la caza. Quieren decir que tenia el sobrenombre de Quijada o Quesada (que en esto hay alguna diferencia en los autores que deste caso escriben), aunque por conjeturas verosimiles se deja entender que se llama Quijana; pero esto importa poco a nuestro cuento; basta que en la narracion del no se salga un punto de la verdad."
+    # texto_de_ejemplo = "En un lugar de la Mancha, de cuyo nombre no quiero acordarme, no ha mucho tiempo que vivia un hidalgo de los de lanza en astillero, adarga antigua, rocin flaco y galgo corredor. Una olla de algo mas vaca que carnero, salpicon las mas noches, duelos y quebrantos los sabados, lentejas los viernes, algun palomino de anadidura los domingos, consumian las tres partes de su hacienda. El resto della concluian sayo de velarte, calzas de velludo para las fiestas con sus pantuflos de lo mismo, los dias de entre semana se honraba con su vellori de lo mas fino. Tenia en su casa una ama que pasaba de los cuarenta, y una sobrina que no llegaba a los veinte, y un mozo de campo y plaza, que asi ensillaba el rocin como tomaba la podadera. Frisaba la edad de nuestro hidalgo con los cincuenta anos, era de complexion recia, seco de carnes, enjuto de rostro; gran madrugador y amigo de la caza. Quieren decir que tenia el sobrenombre de Quijada o Quesada (que en esto hay alguna diferencia en los autores que deste caso escriben), aunque por conjeturas verosimiles se deja entender que se llama Quijana; pero esto importa poco a nuestro cuento; basta que en la narracion del no se salga un punto de la verdad."
 
+    text = text.replace(",", " ,")  # 1. Separar el texto en palabras
+    text_by_words = text.split(' ')
 
-texto_de_ejemplo = texto_de_ejemplo.replace(",", " ,")# 1. Separar el texto en palabras
-text_by_words = texto_de_ejemplo.split(' ')
+    total_lines = []
+    line = ""
+    # 2. Mirar si la palabra cabe en la linea
+    for word in text_by_words:
 
-total_lines = []
-line = ""
-# 2. Mirar si la palabra cabe en la linea
-for word in text_by_words:
-
-    # next_line = False
-    # if (next_line):
-    # while (not next_line):
+        # next_line = False
+        # if (next_line):
+        # while (not next_line):
 
         # tener en cuenta mayúsculas
 
@@ -83,18 +83,36 @@ for word in text_by_words:
 
             # next_line = True
 
-for fire in total_lines:
-    print(len(fire))
+    for fire in total_lines:
+        print(len(fire))
 
-for fire in total_lines:
-    print(fire)
+    for fire in total_lines:
+        print(fire)
 
-# 2. Separar las palabras por letras
-text = list()
-text.append([letter for letter in text_by_words])
+    dot_lines = []
 
-# 3. Traducir a braile
-for word in text:
-    x = braille_utils.middle_signos(word)
-    x = braille_utils.middle_number(x)
-    x = braille_utils.middle_acento(x)
+    for line in total_lines:
+
+        result = []
+        for it, x in enumerate(line):
+
+            x = braille_utils.middle_signos(x)
+            x = braille_utils.middle_number(x)
+            if x.isalnum():
+                x = braille_utils.middle_acento(x.lower())
+
+            if x.isupper():
+                result.append(braille_utils.letras.dict_basic_letter["mayus"])
+
+            if x.isalnum():
+                x = x.lower()
+
+            result.append(braille_utils.letras.dict_basic_letter[x])
+
+        dot_lines.append(braille_utils.extract_first_line(result, 0))
+        dot_lines.append(braille_utils.extract_first_line(result, 1))
+        dot_lines.append(braille_utils.extract_first_line(result, 2))
+
+    return dot_lines
+
+
